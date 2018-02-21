d3172827f11abcc1a2207b94d687f4728862119d
Holden Altaffer <haltaffer@gmail.com>
2018-02-21 22:23:32 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,7 @@
 def oxford_comma(array)
-case array
-when array.length == 1
+if array.length == 1
   array[0]
-when array.length == 2
-  array.join("and")
+elsif array.length == 2
+  array.join(" and ")
 end
 end
